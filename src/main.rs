2fb13d504dc2776d8bd3fed9158600086c913497
Guy Korland <gkorland@gmail.com>
2019-08-12 10:10:44 +0300

    fix loops

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::time::{Instant};
 
-const CAPACITY : usize = 10000;
-const LOOPS : usize = 10;
+const CAPACITY : usize = 100;
+const LOOPS : usize = 1;
 const IN_LOOPS : usize = 50000;
-const ALL_LOOPS: usize = 2;
+const ALL_LOOPS: usize = 1;
 
 fn main() {
     println!("Starting!");
@@ -17,10 +17,10 @@ fn main() {
 
     for _ in 0..ALL_LOOPS {
         println!("Zero test: simple array");
-        for _ in 1..LOOPS {
+        for _ in 0..LOOPS {
             let mut sum = 0;
             let now = Instant::now();
-            for _ in 1..IN_LOOPS {
+            for _ in 0..IN_LOOPS {
                 let mut s = 0;
                 for i in 0..arr.len() {
                     s += arr[i];
@@ -32,10 +32,10 @@ fn main() {
 
 
         println!("First test: Vec loop with index access");
-        for _ in 1..LOOPS {
+        for _ in 0..LOOPS {
             let mut sum = 0;
             let now = Instant::now();
-            for _ in 1..IN_LOOPS {
+            for _ in 0..IN_LOOPS {
                 let mut s = 0;
                 for i in 0..vec.len() {
                     s += vec[i];
@@ -46,10 +46,10 @@ fn main() {
         }
 
         println!("Second test: Vec iterator loop");
-        for _ in 1..LOOPS {
+        for _ in 0..LOOPS {
             let mut sum = 0;
             let now = Instant::now();
-            for _ in 1..IN_LOOPS {
+            for _ in 0..IN_LOOPS {
                 let mut s = 0;
                 for element in vec.iter() {
                     s += element;
@@ -60,10 +60,10 @@ fn main() {
         }
 
         println!("Third test: Vec for_each loop");
-        for _ in 1..LOOPS {
+        for _ in 0..LOOPS {
             let mut sum = 0;
             let now = Instant::now();
-            for _ in 1..IN_LOOPS {
+            for _ in 0..IN_LOOPS {
                 let mut s = 0;
                 vec.iter().for_each(|element| s += element);
                 sum += s;
@@ -72,10 +72,10 @@ fn main() {
         }
 
         println!("Fourth test: Vec fold loop");
-        for _ in 1..LOOPS {
+        for _ in 0..LOOPS {
             let mut sum = 0;
             let now = Instant::now();
-            for _ in 1..IN_LOOPS {
+            for _ in 0..IN_LOOPS {
                 sum += vec.iter().fold(0, |acc, x| acc + x);
             }
             println!("sum:{} time:{:?}", sum, now.elapsed());
